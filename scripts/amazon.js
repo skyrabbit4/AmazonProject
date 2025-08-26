03b3befe1acc7708c56e81e0828954f9a39a6a19
shubham.kaushik004@gmail.com <shubham.kaushik004@gmail.com>
2025-08-26 23:33:10 +0530

    Add data attribute to "Add to Cart" button and clean up event listener

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -48,7 +48,8 @@ products.forEach((product)=>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-name="${product.name}">
             Add to Cart
           </button>
         </div>`;
@@ -57,7 +58,7 @@ products.forEach((product)=>{
         document.querySelectorAll('.js-add-to-cart')
         .forEach((button)=>{
             button.addEventListener('click',()=>{
-                console.log('Add to cart clicked'); 
+                
             })
         })
 
