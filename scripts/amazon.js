b64d18465145ac8771c4c9e347bbb79119b99197
shubham.kaushik004@gmail.com <shubham.kaushik004@gmail.com>
2025-08-30 00:34:22 +0530

    Update cart quantity display and improve product HTML structure

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,58 +1,56 @@
-
-
-let productsHtml='';
+let productsHtml = '';
 
 products.forEach((product)=>{
     productsHtml=productsHtml+`
     <div class="product-container">
-          <div class="product-image-container">
+      <div class="product-image-container">
             <img class="product-image"
               src="${product.image}">
-          </div>
+      </div>
 
-          <div class="product-name limit-text-to-2-lines">
-           ${product.name}
-          </div>
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
 
-          <div class="product-rating-container">
+      <div class="product-rating-container">
             <img class="product-rating-stars"
               src="images/ratings/rating-${product.rating.stars*10}.png">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
-            </div>
-          </div>
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
 
-          <div class="product-price">
+      <div class="product-price">
             ${(product.priceCents/100).toFixed(2)}
-          </div>
+      </div>
 
-          <div class="product-quantity-container">
-            <select>
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
+      <div class="product-quantity-container">
+        <select>
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
 
-          <div class="product-spacer"></div>
+      <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
+      <div class="added-to-cart">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
 
           <button class="add-to-cart-button button-primary js-add-to-cart"
           data-product-id="${product.id}">
-            Add to Cart
-          </button>
-        </div>`;
+        Add to Cart
+      </button>
+    </div>`;
 
         document.querySelector('.js-products-grid').innerHTML=productsHtml;
         document.querySelectorAll('.js-add-to-cart')
@@ -64,24 +62,27 @@ products.forEach((product)=>{
               cart.forEach((item)=>{
                 if(productId===item.productId){
                   matchingItem=item;
-                 
+
                 }
               });
               if(matchingItem){
                 matchingItem.quantity+=1;
               }
               else{
-                cart.push({
+      cart.push({
                   productId:productId,
                   quantity:1
-                });
-
+      });
+      
               }
+              let   cartQuantity=0;
+      cart.forEach((item)=>{
+        cartQuantity=cartQuantity+item.quantity;
+      })
+              console.log('cartQuantity', cartQuantity);
               console.log(cart);
-                
+
+            document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
             });
-        });
-
-
-
-})
+          });
+        })
