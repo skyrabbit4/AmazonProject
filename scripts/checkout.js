a35dc34502ff8df97871efbfe81214f44070169d
shubham.kaushik004@gmail.com <shubham.kaushik004@gmail.com>
2025-08-30 21:19:05 +0530

    Update cart item display and remove item functionality in checkout script

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -22,7 +22,7 @@ cart.forEach((cartItem)=>{
 
     
     cartSummaryHTML+=
-    `<div class="cart-item-container">
+    `<div class="cart-item-container js-cart-item-container-${matchingproduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -107,6 +107,7 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click',()=>{
        const productId =link.dataset.productId
        removeFromCart(productId);
-       console.log(cart);
+      document.querySelector(`.js-cart-item-container-${productId}`).remove();
+
     })
 })
