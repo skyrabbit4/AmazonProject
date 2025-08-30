1275d330c6ccc225ec866357a31ece62b6087ddb
shubham.kaushik004@gmail.com <shubham.kaushik004@gmail.com>
2025-08-30 19:11:20 +0530

    Add formatCurrency utility and update price display in product and checkout scripts

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 
 import {cart} from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML='';
 
@@ -34,8 +35,8 @@ cart.forEach((cartItem)=>{
                     ${matchingproduct.name}
                 </div>
                 <div class="product-price">
-                  $${matchingproduct.priceCents/100}
-                </div>
+                  $${formatCurrency(matchingproduct.priceCents)}
+                </div
                 <div class="product-quantity">
                   <span>
                     Quantity: <span class="quantity-label">${cartItem.quantity}</span>
@@ -56,7 +57,7 @@ cart.forEach((cartItem)=>{
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingproduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -69,7 +70,7 @@ cart.forEach((cartItem)=>{
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingproduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -82,7 +83,7 @@ cart.forEach((cartItem)=>{
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingproduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
