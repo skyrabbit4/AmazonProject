1c6af0e43fc26c4ca1e84a1de30aa90001e12b76
shubham.kaushik004@gmail.com <shubham.kaushik004@gmail.com>
2025-08-31 00:15:31 +0530

    Add delivery date calculation and update money utility export

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,15 @@
 import {cart,removeFromCart} from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
+const today = dayjs();
+
+hello();
+
+
+const deliveryDate=today.add(7,'days');
+console.log(deliveryDate.format('dddd, MMMM D'));
 
 
 let cartSummaryHTML='';
