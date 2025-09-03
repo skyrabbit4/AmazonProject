f68bfa0681cea368ae5c598ac466ca92483a03c6
shubham.kaushik004@gmail.com <shubham.kaushik004@gmail.com>
2025-09-03 23:17:09 +0530

    Add additional test case for rounding up in formatCurrency function

--- tests-jasmine/moneyTests.js
+++ tests-jasmine/moneyTests.js
@@ -8,6 +8,12 @@ describe('test suite : formatCurrency',()=>{
     it('works with 0',()=>{
         expect(formatCurrency(0)).toEqual('0.00');
     });
+
+    it('round up to the nearest cent',()=>{
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+
+    
 });
 
 
