b1d8e9c75405bac1c3315500b162f6d30a317b14
shubham.kaushik004@gmail.com <shubham.kaushik004@gmail.com>
2025-09-06 01:45:54 +0530

    Refactor Cart class: encapsulate localStorageKey and loadFromStorage as private properties/methods

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,15 +1,16 @@
 
 class Cart{
-    cartItems;
-    localStorageKey;
+    cartItems; //public property, accessible from outside
+    #localStorageKey; //private property
 
     constructor(localStorageKey){
-      this.localStorageKey=localStorageKey;
-      this.loadFromStorage();
+      this.#localStorageKey=localStorageKey;
+      this.#loadFromStorage();
      }
  
-      loadFromStorage()  {
-      this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+     //private method
+      #loadFromStorage()  {
+      this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
       if (!this.cartItems) {
       this.cartItems = [{
         productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -23,7 +24,7 @@ class Cart{
     }
   }
   saveToStorage() {
-    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
 
    addToCart(productId) {
@@ -70,6 +71,7 @@ class Cart{
 //using a function to genrate cart objects
 const cart= new Cart('cart-oop');
 const businessCart= new Cart('cart-business');
+
 
 
 
