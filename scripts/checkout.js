c9a7c96e9ad6973024b5fdc69d26188cce5dd534
shubham.kaushik004@gmail.com <shubham.kaushik004@gmail.com>
2025-09-07 16:04:08 +0530

    refactor loadPage function to use async/await for improved readability

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,19 @@ import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js'
 // import '../data/backend-practice.js'
 
+async function loadPage(){
+  await loadProductsFetch();
+  await new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1');
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
@@ -22,7 +35,8 @@ Promise.all([
   renderOrderSummary();
      renderPaymentSummary();
 })
-/*
+     */
+    /*
 new Promise((resolve) => {
   loadProducts(() => {
     resolve();
